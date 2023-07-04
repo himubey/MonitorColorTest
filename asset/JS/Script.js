078a726813b80fdf079e3a5f3c223b7eecd56bef
Himubey <himubey@hotmail.com>
2023-07-04 21:05:44 +0530

    feat: add two more colors

--- asset/JS/Script.js
+++ asset/JS/Script.js
@@ -15,5 +15,14 @@ buttons.forEach(function(button){
         if(event.target.id === 'blue'){
             body.style.backgroundColor = event.target.id
         }
+        if(event.target.id === 'white'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id === 'gray'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id === 'black'){
+            body.style.backgroundColor = event.target.id
+        }
     })
 })
